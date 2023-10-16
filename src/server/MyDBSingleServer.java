6ad7312605a41c2165ad28b8406f72936df69cc5
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-15 20:00:27 -0400

    add MyDBSingleServer

--- src/server/MyDBSingleServer.java
+++ src/server/MyDBSingleServer.java
@@ -9,13 +9,48 @@ import java.net.InetSocketAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.datastax.driver.core.*;
+
+
 /**
  * This class should implement the logic necessary to perform the requested
  * operation on the database and return the response back to the client.
  */
 public class MyDBSingleServer extends SingleServer {
+    private final Cluster cluster;
+    private final Session session;
+
     public MyDBSingleServer(InetSocketAddress isa, InetSocketAddress isaDB,
                             String keyspace) throws IOException {
         super(isa, isaDB, keyspace);
+        this.cluster = Cluster.builder().addContactPoint(isaDB.getHostName()).withPort(isaDB.getPort()).build();
+        this.session = cluster.connect(keyspace);
+    }
+
+    @Override
+    protected void handleMessageFromClient(byte[] bytes, NIOHeader header) {
+        String request = new String(bytes);
+        String response;
+
+        try {
+            ResultSet results = session.execute(request);
+            // For simplicity, just returning the first row as response
+            if (!results.isExhausted()) {
+                Row row = results.one();
+                response = row.toString();
+            } else {
+                response = "Operation executed successfully, but no data to return.";
+            }
+        } catch (Exception e) {
+            response = "Error executing operation: " + e.getMessage();
+        }
+
+        try {
+            log.log(Level.INFO, "{0} received message from {1}", new Object[]
+                    {this.clientMessenger.getListeningSocketAddress(), header.sndr});
+            this.clientMessenger.send(header.sndr, response.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

b22373f10dd23d6a858b0ae9bd3cec81fed00cae
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-29 16:23:48 -0400

    update MyDBClient+server

--- src/client/MyDBClient.java
+++ src/client/MyDBClient.java
@@ -1,57 +1,86 @@
 package client;
 
 import edu.umass.cs.nio.interfaces.NodeConfig;
-import server.SingleServer;
 import edu.umass.cs.nio.nioutils.NIOHeader;
+import org.json.JSONException;
+import org.json.JSONObject;
+import server.SingleServer;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This class should implement your DB client.
+ */
 public class MyDBClient extends Client {
-    private NodeConfig<String> nodeConfig = null;
-
-    private final HashMap<String, Callback> callbackMap = new HashMap<String, Callback>();
-
-    public MyDBClient() throws IOException {
-        super();
-    }
+    private NodeConfig<String> nodeConfig= null;
+    long reqnum = 0;
+    ConcurrentHashMap<Long, Callback> callbacks = new ConcurrentHashMap<Long,
+            Callback>();
 
     public MyDBClient(NodeConfig<String> nodeConfig) throws IOException {
         super();
         this.nodeConfig = nodeConfig;
     }
 
-    @Override
+    /** TODO: This method will automatically get invoked whenever any response
+     * is received from a remote node. You need to implement logic here to
+     * ensure that whenever a response is received, the callback method
+     * that was supplied in callbackSend(.) when the corresponding request
+     * was sent is invoked.
+     *
+     * Extend this method in MyDBClient to implement your logic there. This
+     * file will be overwritten to the original in your submission.
+     *
+     * @param bytes The content of the received response
+     * @param header Sender and receiver information about the received response
+     */
     protected void handleResponse(byte[] bytes, NIOHeader header) {
+        // expect echo reply here
         try {
-            String response = new String(bytes, SingleServer.DEFAULT_ENCODING);
-            String[] parts = response.split(":::", 2);
-            String actualResponse = parts[0];
-            String requestID = parts[1];
+            JSONObject response = new JSONObject(new String(bytes, SingleServer
+                    .DEFAULT_ENCODING));
+            Callback callback = callbacks.get(response.getLong(Keys
+                    .REQNUM.toString()));
+            if(callback!=null)
+                callback.handleResponse(bytes, header);
 
-            if  (requestID.length() > 0) {
-                Callback callback = callbackMap.remove(requestID);
-                if (callback != null) {
-                    callback.handleResponse(actualResponse.getBytes(SingleServer.DEFAULT_ENCODING), header);
-                } else {
-                    super.handleResponse(bytes, header);
-                }
-            }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            //e.printStackTrace();
         }
     }
 
-    @Override
-    public void callbackSend(InetSocketAddress isa, String request, Callback callback) throws IOException {
+    public static enum Keys {
+        REQNUM, TYPE, REQUEST, RESPONSE;
+    }
+    private synchronized long getUniqueIdentifier(String request) {
+        return reqnum++;
+    }
+
+    /**
+     * TODO: This method, unlike the simple send above, should invoke the
+     * supplied callback's handleResponse method upon receiving the
+     * corresponding response from the remote end.
+     *
+     * Extend this method in MyDBClient to implement your logic there. This
+     * file will be overwritten to the original in your submission.
+     *
+     * @param isa
+     * @param request
+     * @param callback
+     */
+    public void callbackSend(InetSocketAddress isa, String request,
+                             Callback callback) throws IOException {
         try {
-            String requestID = String.valueOf(System.currentTimeMillis());
-            String modifiedRequest = request + ":::" + requestID;
-            callbackMap.put(requestID, callback);
-            send(isa, modifiedRequest);
-        } catch (IOException e) {
+            JSONObject json = new JSONObject().put(Keys.REQNUM.toString(),
+                    getUniqueIdentifier(request)).put(Keys.REQUEST.toString(), request);
+            this.callbacks.put(json.getLong(Keys.REQNUM.toString()), callback);
+            this.send(isa, json.toString());
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
--- src/server/MyDBSingleServer.java
+++ src/server/MyDBSingleServer.java
@@ -3,6 +3,8 @@ package server;
 import edu.umass.cs.nio.AbstractBytePacketDemultiplexer;
 import edu.umass.cs.nio.MessageNIOTransport;
 import edu.umass.cs.nio.nioutils.NIOHeader;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -11,45 +13,68 @@ import java.util.logging.Logger;
 
 import com.datastax.driver.core.*;
 
+import client.MyDBClient;
+
 
 /**
  * This class should implement the logic necessary to perform the requested
  * operation on the database and return the response back to the client.
  */
 public class MyDBSingleServer extends SingleServer {
-    private final Cluster cluster;
-    private final Session session;
+    final private Session session;
+    final private Cluster cluster;
 
     public MyDBSingleServer(InetSocketAddress isa, InetSocketAddress isaDB,
                             String keyspace) throws IOException {
         super(isa, isaDB, keyspace);
-        this.cluster = Cluster.builder().addContactPoint(isaDB.getHostName()).withPort(isaDB.getPort()).build();
-        this.session = cluster.connect(keyspace);
+        session = (cluster=Cluster.builder().addContactPoint("127.0.0.1")
+                .build()).connect("demo");
     }
 
     @Override
     protected void handleMessageFromClient(byte[] bytes, NIOHeader header) {
-        String request = new String(bytes);
-        String response;
-        String[] parts = request.split(":::", 2);
-        String actualRequest = parts[0];
-        String requestID = "";
-        if (parts.length >= 2) {
-            requestID = parts[1];
-        }
+        // simple echo server
         try {
-            session.execute(actualRequest);
-            response = "success:::" + requestID;
-        } catch (Exception e) {
-            response = "failed:::" + requestID;
-        }
+            log.log(Level.INFO, "{0} received message from {1} {2}", new
+                    Object[]
+                    {this.clientMessenger.getListeningSocketAddress(), header
+                            .sndr, new String(bytes)});
+            String request = new String(bytes, SingleServer.DEFAULT_ENCODING);
+            JSONObject json = null;
+            try {
+                json = new JSONObject(request);
+                request = json.getString(MyDBClient.Keys.REQUEST
+                        .toString());
+            } catch (JSONException e) {
+                //e.printStackTrace();
+            }
 
-        try {
-            log.log(Level.INFO, "{0} received message from {1}", new Object[]
-                    {this.clientMessenger.getListeningSocketAddress(), header.sndr});
-            this.clientMessenger.send(header.sndr, response.getBytes());
+            ResultSet results = session.execute(request);
+            String response="";
+            for(Row row : results) {
+                response += row.toString();
+            }
+            if(json!=null) try {
+                json.put(MyDBClient.Keys.RESPONSE.toString(),
+                        response);
+                response = json.toString();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            System.out.println(this.clientMessenger
+                    .getListeningSocketAddress() + " executed " +
+                    request + " and sending response " +"["+response+"]");
+            this.clientMessenger.send(header.sndr, response.getBytes
+                    (SingleServer.DEFAULT_ENCODING));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void close() {
+        super.close();
+        session.close();
+        cluster.close();
     }
 }
